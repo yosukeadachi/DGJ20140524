68ee425d290336a9db04f7f3c119b8e034217e55
yosukeadachi <yosukeadachi@gmail.com>
2014-05-24 13:46:45 +0900

    現状コミット

---
+++ project/Assets/Script/Item.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public AudioClip audioClip;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource>();
+		audioSource.clip = audioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision2d)
+	{
+		GameObject go = collision2d.gameObject;
+		if(go.tag == "Player") {
+			audioSource.Play();
+		}
+	}
+
+}
