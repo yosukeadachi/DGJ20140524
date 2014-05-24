eb8d62bbc7e05f8c86a9ea735c2629724fb263f3
yosukeadachi <yosukeadachi@gmail.com>
2014-05-24 14:50:09 +0900

    現状コミット

---
+++ project/Assets/Script/Score.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	// スコアを表示するGUIText
+	public GUIText scoreGUIText;
+	
+	// targetスコアを表示するGUIText
+	public GUIText targetScoreGUIText;
+	
+	// スコア
+	private int score;
+	
+	//   target
+	private int targetScore;
+	
+	// PlayerPrefsで保存するためのキー
+	private string highScoreKey = "highScore";
+	
+	void Start ()
+	{
+		Initialize ();
+	}
+	
+	void Update ()
+	{
+		// スコアがtargetスコアより大きければ
+		if (targetScore < score) {
+			Debug.Log ("clear");
+		}
+		
+		// スコア・ハイスコアを表示する
+		scoreGUIText.text = "Score : " + score.ToString ();
+		targetScoreGUIText.text = "TargetScore : " + targetScore.ToString ();
+	}
+	
+	// ゲーム開始前の状態に戻す
+	private void Initialize ()
+	{
+		// スコアを0に戻す
+		score = 0;
+		
+		// 
+		targetScore = 100;
+	}
+	
+	// ポイントの追加
+	public void AddPoint (int point)
+	{
+		score = score + point;
+	}
+	
+}
+
+
