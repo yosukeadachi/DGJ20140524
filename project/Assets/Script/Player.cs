993824c1e6cc6f8670eabaa1a47e2d3d848f0fb3
yosukeadachi <yosukeadachi@gmail.com>
2014-05-24 17:58:23 +0900

    スタミナ実装

--- project/Assets/Script/Player.cs
+++ project/Assets/Script/Player.cs
@@ -9,24 +9,50 @@ public class Player : MonoBehaviour {
 	AudioSource audioSource;
 	public int point = 10;
 
+	public static float STAMINA_MAX = 100.0f;
+	public float stamina = 0.0f;
+	public float stamina_delta = 0.0f;
+	public float stamina_up_y = 0.0f;
+	public static float STAMINA_DRAW_SCALE_DEF = 0.4161998f;
+
+
 	// Use this for initialization
 	void Start () {
 		audioSource = gameObject.GetComponent<AudioSource>();
+		stamina = STAMINA_MAX;
+		stamina_delta = 2.5f;
+		stamina_up_y = -3.135001f;
+		point = 10;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetMouseButton(0)){
-//			Debug.Log("left");
-			int x = 0;
-			int y = +50;
-			int z = 0;
-			this.gameObject.transform.rigidbody2D.AddForce(new Vector3(x, y, z));
+			if(stamina > 0) {
+				stamina -= stamina_delta;
+				if(stamina < 0) {
+					stamina = 0;
+				}
+	//			Debug.Log("left");
+				int x = 0;
+				int y = +50;
+				int z = 0;
+				this.gameObject.transform.rigidbody2D.AddForce(new Vector3(x, y, z));
+			}
 		}
 		if (Input.GetMouseButtonDown(0)) {
 			audioSource.PlayOneShot (audioJump);
 		}
+
+		//stamina_up
+		if(this.gameObject.transform.position.y < stamina_up_y) {
+			stamina = STAMINA_MAX;
+		}
+
+		//stamina_draw
+		GameObject.Find("stamina_2").transform.localScale = new Vector3(0.4161998f, STAMINA_DRAW_SCALE_DEF*stamina/STAMINA_MAX, 1f);
+
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision2d)
